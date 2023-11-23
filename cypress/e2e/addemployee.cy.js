0adb7b472015120c040f3fe1a2ff3487961378fe
Madan20232023 <madans6218@gmail.com>
2023-11-23 17:16:14 +0530

    feature branch changes

--- cypress/e2e/addemployee.cy.js
+++ cypress/e2e/addemployee.cy.js
@@ -1,12 +1,12 @@
 describe('verify add employee functionality', () => {
     it('verify add employee with valid details', () =>{
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('input[placeholder="Username"]').type('Admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('button[type="submit"]').click()
-    cy.url().should("eq",'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
-    cy.contains('Dashboard').should("be.visible")
-    cy.contains('PIM').click()
+    // cy.get('input[placeholder="Username"]').type('Admin')
+    // cy.get('input[name="password"]').type('admin123')
+    // cy.get('button[type="submit"]').click()
+    // cy.url().should("eq",'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+    // cy.contains('Dashboard').should("be.visible")
+    // cy.contains('PIM').click()
     cy.contains('Add Employee').click()
     cy.get('input[placeholder="First Name"]').type("madan")
     cy.get('input[placeholder="Last Name"]').type("s")
